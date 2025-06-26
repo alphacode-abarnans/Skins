877c46e667d089f4e4fdfc39dec1b81e9c404015
alphacode-abarnans <abarnans@alphacodetechnologies.com>
2025-06-26 10:59:28 +0530

    button component put in folders to check it's import and fetch

---
+++ src/components/skins/buttons/button.tsx
@@ -0,0 +1,20 @@
+import { ButtonProps, com } from "@qbit.design/shell";
+
+
+const ButtonSkin2: com.qbit.Skin<ButtonProps> = (props: com.qbit.SkinProps<ButtonProps>) => {
+  const { value, disabled, tabIndex, testId, onClick } = props;
+
+  return (
+    <button
+      className={`py-2 px-4 rounded-md bg-gray-200 font-medium text-gray-800 ${disabled ? 'opacity-60' : 'hover:bg-gray-300'}`}
+      tabIndex={tabIndex}
+      disabled={disabled}
+      onClick={onClick}
+      data-testid={testId}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default ButtonSkin2;
