06ece7245f31fe1b980bb03f3f63b299412af414
alphacode-abarnans <abarnans@alphacodetechnologies.com>
2025-07-03 11:09:15 +0530

    important removed

--- src/components/skins/button.tsx
+++ src/components/skins/button.tsx
@@ -7,7 +7,7 @@ const ButtonSkin: com.qbit.Skin<ButtonProps> = (props: com.qbit.SkinProps<Button
 
   return (
     <button
-      className={`py-2 px-4 rounded-md !bg-blue-300 font-medium !text-gray-800 ${disabled ? 'opacity-60' : 'hover:!bg-gray-300'}`}
+      className={`py-2 px-4 rounded-lg bg-blue-300 font-medium text-blue-800 ${disabled ? 'opacity-60' : 'hover:bg-green-600 text-white'}`}
       tabIndex={tabIndex}
       disabled={disabled}
       onClick={onClick}
