27509a73c139efeec4389e62b09c8a0b3493dfad
Abarnan Suresh <abarnans@alphacodetechnologies.com>
2025-07-08 11:22:17 +0530

    Update button.tsx

--- src/components/skins/buttons/button.tsx
+++ src/components/skins/buttons/button.tsx
@@ -3,11 +3,11 @@ import { ButtonProps, com } from "@qbit.design/shell";
 
 
 const ButtonSkin2: com.qbit.Skin<ButtonProps> = (props: com.qbit.SkinProps<ButtonProps>) => {
-  const { value, disabled, tabIndex, testId, onClick } = props;
+  const { value, disabled, tabIndex, testId, onClick, className } = props;
 
   return (
     <button
-      className={`py-2 px-4 rounded-md !bg-gray-200 font-medium !text-gray-800 ${disabled ? 'opacity-60' : 'hover:!bg-gray-300'}`}
+      className={`py-2 px-4 rounded-md !bg-gray-200 font-medium !text-gray-800 ${className} ${disabled ? 'opacity-60' : 'hover:!bg-gray-300'}`}
       tabIndex={tabIndex}
       disabled={disabled}
       onClick={onClick}
